1fae8903792490651f9f1c001ef639951bf290d7
TomJoe <Tom.Joe.1984@googlemail.com>
2014-12-01 23:03:17 +0100

    nicer Colors , more Black

--- LEDQuailizer_FastLED.ino
+++ LEDQuailizer_FastLED.ino
@@ -1,13 +1,13 @@
 #include <FastLED.h>
 #include <AudioAnalyzer.h>
 
-//#define RENE //so lassen, dann sollte es klappen
+//                                        #define RENE //so lassen, dann sollte es klappen
 
 #ifdef RENE
 
 #define DATA_PIN      5 //Anpassen
 #define COLOR_ORDER   GRB //Anpassen
-#define CHIPSET       WS2811 //CHIPSET anpassen
+#define CHIPSET       WS2811 //CHIPSET anpassen  
 #define NUM_LEDS      50 //Anpassen
 
 #else
@@ -23,9 +23,8 @@
 
 #define BRIGHTNESS  255
 #define FRAMES_PER_SECOND 50
-#define DECAY_PER_FRAME 0.5 //mit dem Wert kannst rumspielen 0.0 - 1.0
-#define MIN_CUT_OFF_LEVEL 100 //mit dem Wert kannst rumspielen - 1024
-#define INI_CUT_OFF_LEVEL 100  // momentan funktionslos
+#define DECAY_PER_FRAME 0.1 //mit dem Wert kannst rumspielen 0.0 - 1.0
+#define MIN_CUT_OFF_LEVEL 50 //mit dem Wert kannst rumspielen - 1024
 #define CUT_OFF_DECY_PER_FRAME 1 // momentan funktionslos
 
 CRGB leds[NUM_LEDS];
@@ -33,11 +32,11 @@ CRGBPalette16 gPal, nPal;
 
 #ifdef RENE
 
-  Analyzer Audio = Analyzer(6,7,0); //Strobe pin ->6  RST pin ->7 Analog Pin ->0 //Anpassen
+Analyzer Audio = Analyzer(6, 7, 0); //Strobe pin ->6  RST pin ->7 Analog Pin ->0 //Anpassen
 
 #else
 
-  Analyzer Audio = Analyzer(4,3,A5)   ; //Strobe pin ->4  RST pin ->3 Analog Pin ->5 //Anpassen
+Analyzer Audio = Analyzer(4, 3, A5)   ; //Strobe pin ->4  RST pin ->3 Analog Pin ->5 //Anpassen
 
 #endif
 
@@ -48,19 +47,19 @@ int FreqValCutOff[7];
 void setup() {
   //delay(3000); // sanity delay
   Serial.begin(57600);
-  
-  #ifdef RENE
-  
-  FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);  
-  
-  #else
-  
+
+#ifdef RENE
+
+  FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+
+#else
+
   FastLED.addLeds<CHIPSET, LED_PIN, CLK_PIN, COLOR_ORDER, SPI_SPEED>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  
-  #endif
-  
+
+#endif
+
   FastLED.setBrightness( BRIGHTNESS );
-  for(int i = 0; i<7; i++) FreqValCutOff[i] = INI_CUT_OFF_LEVEL;
+  for (int i = 0; i < 7; i++) FreqValCutOff[i] = MIN_CUT_OFF_LEVEL;
   //gPal = HeatColors_p;
   gPal = PartyColors_p;
   Audio.Init();
@@ -80,38 +79,74 @@ void loop()
 void AudioFire() {
   static int frame = 0;
   frame++;
-  
+
   Audio.ReadFreq(FreqVal);
   fill_solid(leds, NUM_LEDS, 0);
-  
-  for(int i = 0; i < 7; i++){
-    if((FreqVal[i] - FreqValCutOff[i]) > FreqValLevel[i]){
-      FreqValLevel[i] = FreqVal[i] - FreqValCutOff[i];
-      //FreqValCutOff[i] = FreqVal[i] / 2;
+
+  for (int i = 0; i < 7; i++) {
+    if (FreqVal[i] > (1.5 * FreqValCutOff[i])) {
+      FreqValLevel[i] = 128;
     }
-    Serial.print(max(FreqVal[i]-FreqValCutOff[i],0));//Transimit the DC value of the seven bands
-    if(i<6)  Serial.print(",");
+
+    FreqValCutOff[i] = ((FRAMES_PER_SECOND - 1) * FreqValCutOff[i] + FreqVal[i]) / FRAMES_PER_SECOND;
+    if (FreqValCutOff[i] < MIN_CUT_OFF_LEVEL) FreqValCutOff[i] = MIN_CUT_OFF_LEVEL;
+
+    Serial.print(FreqValCutOff[i]);//Transimit the DC value of the seven bands
+    if (i < 6)  Serial.print(",");
     else Serial.println();
-    
-    double middleIdx = (NUM_LEDS/14.0)*(1+2*i);
-    
-    //double pulseLength = FreqValCache[i] / 50;
-    double pulseLength = NUM_LEDS/14 +2;
-      
-    //double palStart = 240.0; //good in FirePalette
-    double palStart = 32 * i;
-    //double palStep = -abs((240.0/(pulseLength))); 
-    //double palStep = abs((50.0/(pulseLength))); 
-    double palStep = 0;
+
+    double middleIdx;
+    double pulseLength;
+    double palStart;
+    double palStep = -1.0;
     double brightnessDecay = 0.5;
 
-    fill_palette_float(leds , middleIdx , middleIdx + pulseLength , palStart, palStep, gPal, FreqValLevel[i] , brightnessDecay,BLEND);
-    fill_palette_float(leds , middleIdx , middleIdx - pulseLength , palStart, palStep, gPal, FreqValLevel[i] , brightnessDecay,BLEND);
-    
+
+    switch (i) {
+      case 0:
+      case 1:
+        middleIdx = (NUM_LEDS / 4) * (1 + i*2);
+        pulseLength = (NUM_LEDS / 4);
+        palStart = 255;
+        palStep = -5.0;
+        brightnessDecay = 0.5;
+        break;
+
+      case 2:
+  
+      case 3:
+     
+      case 4:
+        
+      case 5:
+        
+      case 6:
+        middleIdx = (NUM_LEDS / 10.0) * ((2 * i) - 3);
+        pulseLength = NUM_LEDS / 5;
+        palStart = 128;
+        palStep = -10.0;
+        brightnessDecay = 0.4;
+        break;
+    }
+    //double middleIdx = (NUM_LEDS / 14.0) * (1 + 2 * i);
+
+    //double pulseLength = FreqValCache[i] / 50;
+    //double pulseLength = NUM_LEDS / 14;
+
+    //double palStart = 240.0; //good in FirePalette
+    //double palStart = 32 * i;
+    //double palStep = -abs((240.0/(pulseLength)));
+    //double palStep = abs((50.0/(pulseLength)));
+    //double palStep = 0;
+    //double brightnessDecay = 0.5;
+
+    fill_palette_float(leds , middleIdx , middleIdx + pulseLength , palStart, palStep, gPal, FreqValLevel[i] , brightnessDecay, BLEND);
+    fill_palette_float(leds , middleIdx , middleIdx - pulseLength , palStart, palStep, gPal, FreqValLevel[i] , brightnessDecay, BLEND);
+
     FreqValLevel[i] =  FreqValLevel[i] * DECAY_PER_FRAME;
-    if(FreqValLevel[i] < 0) FreqValLevel[i] = 0; 
+    if (FreqValLevel[i] < 0) FreqValLevel[i] = 0;
     //FreqValCutOff[i] -= CUT_OFF_DECY_PER_FRAME;
-    //if(FreqValLevel[i] < MIN_CUT_OFF_LEVEL) FreqValLevel[i] = MIN_CUT_OFF_LEVEL; 
+    //if(FreqValLevel[i] < MIN_CUT_OFF_LEVEL) FreqValLevel[i] = MIN_CUT_OFF_LEVEL;
   }
 
 }
@@ -122,13 +157,13 @@ void fill_palette_float(CRGB* pLeds , int pLedStartIdx , int pLedEndIdx , double
 
   if (pLedStartIdx < pLedEndIdx) {
     for (; pLedStartIdx < pLedEndIdx; pLedStartIdx++) {
-      if(pLedStartIdx >= 0 && pLedStartIdx < NUM_LEDS) pLeds[pLedStartIdx] =  pLeds[pLedStartIdx] + ColorFromPalette( pGPal, pPalStart, pBrightness, blendType);
+      if (pLedStartIdx >= 0 && pLedStartIdx < NUM_LEDS) pLeds[pLedStartIdx] =  pLeds[pLedStartIdx] + ColorFromPalette( pGPal, pPalStart, pBrightness, blendType);
       pPalStart += pPalStep;
       pBrightness *= pBrightnessDecay;
     }
   } else {
     for (; pLedStartIdx > pLedEndIdx; pLedStartIdx--) {
-      if(pLedStartIdx >= 0 && pLedStartIdx < NUM_LEDS) pLeds[pLedStartIdx] = pLeds[pLedStartIdx] + ColorFromPalette( pGPal, pPalStart, pBrightness, blendType);
+      if (pLedStartIdx >= 0 && pLedStartIdx < NUM_LEDS) pLeds[pLedStartIdx] = pLeds[pLedStartIdx] + ColorFromPalette( pGPal, pPalStart, pBrightness, blendType);
       pPalStart += pPalStep;
       pBrightness *= pBrightnessDecay;
     }
